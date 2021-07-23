afa06b84c804326f67723cca937e9e3d7b6c29f2
yuuki291 <yuuki1948@outlook.jp>
2021-07-23 12:40:47 +0900

    v_1.0

---
+++ src/main/java/com/rugbyaholic/spring_project/cof/RootController.java
@@ -0,0 +1,14 @@
+package com.rugbyaholic.spring_project.cof;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+
+	@RequestMapping("/")
+	public String root() {
+		return "index";
+	}
+
+}
